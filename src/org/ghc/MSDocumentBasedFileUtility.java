c6947887e738b3667d2290c90a45145c8aac4d82
Pitambar Mohanty <pitambarm@pune.smartek21.st21>
2016-11-03 18:11:21 +0530

    remote previous code

--- src/org/ghc/MSDocumentBasedFileUtility.java
+++
@@ -1,5 +0,0 @@
-package org.ghc;
-
-public class MSDocumentBasedFileUtility {
-
-}
--- src/org/ghc/PDFFileBasedFileUtility.java
+++
@@ -1,8 +0,0 @@
-package org.ghc;
-
-import org.apache.log4j.Logger;
-
-public class PDFFileBasedFileUtility {
-	private static final Logger LOGGER = Logger.getLogger(PDFFileBasedFileUtility.class);
-
-}
